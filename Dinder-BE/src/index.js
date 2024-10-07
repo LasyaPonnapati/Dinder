cac5591209a68f36ae520d6554bda3e2dce04e92
LasyaPonnapati <lasyaponnapati@gmail.com>
2024-10-07 13:13:27 +0530

    dynamically sending data to an api

--- Dinder-BE/src/index.js
+++ Dinder-BE/src/index.js
@@ -3,13 +3,10 @@ const connectDB = require("./config/Database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());//built-in middleware function in Express - parses incoming req json data 
+
 app.post("/signup",async(req,res)=>{
-    const user = new User({
-        firstName: "Lasya",
-        lastName: "Reddy",
-        emailId: "lasya@reddy.com",
-        password: "lasya@123"
-    });
+    const user = new User(req.body);
     await user.save();
     console.log("data inserted into user collection");
     res.send("user added");
