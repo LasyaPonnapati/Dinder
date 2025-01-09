c49050fb482d4861ca5663198419be81d02d0d5e
LasyaPonnapati <lasyaponnapati@gmail.com>
2025-01-09 12:13:05 +0530

    Navbar

--- Dinder-FE/UI/src/App.jsx
+++ Dinder-FE/UI/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <NavBar />
       <h1 className='text-3xl underline'>Hello World!</h1>
     </>
   )
